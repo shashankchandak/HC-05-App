0a5900ce02debf8bc7f8233e94c99386d310c655
Shashank Chandak <chandak.shashank16@gmail.com>
2020-02-07 21:34:22 +0530

    Data successfully sent to ubidots server.

--- app/src/main/java/com/example/danyal/bluetoothhc05/ledControl.java
+++ app/src/main/java/com/example/danyal/bluetoothhc05/ledControl.java
@@ -15,12 +15,27 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.ubidots.ApiClient;
+import com.ubidots.Variable;
+
 import org.w3c.dom.Text;
 
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ledControl extends AppCompatActivity {
 
@@ -32,6 +47,8 @@ public class ledControl extends AppCompatActivity {
     BluetoothSocket btSocket = null;
     private boolean isBtConnected = false;
     static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    Map<String,String> variableLabels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,6 +96,13 @@ public class ledControl extends AppCompatActivity {
                 Disconnect();
             }
         });
+        variableLabels=new HashMap<>();
+        variableLabels.put("a","https:-industrial.ubidots.com-app-devices-5e3d46c01d8472537b2d6106-undefinednew-variable");
+        variableLabels.put("b","new-variable");
+        variableLabels.put("c","c");
+        variableLabels.put("d","d");
+
+
     }
 
     private void sendSignal ( String number ) {
@@ -92,23 +116,21 @@ public class ledControl extends AppCompatActivity {
                 byte[] buffer = new byte[256];
                 int bytes;
                 bytes=dataInputStream.read(buffer);
-                String readMessage = new String(buffer, 0, bytes);
-                Log.i("data",readMessage);
-                display_data.setText(readMessage);
+                String receivedMessage = new String(buffer, 0, bytes);
+                Log.i("data",receivedMessage);
+                display_data.setText(receivedMessage);
+                String[] updates;
+                updates=receivedMessage.split("\n");
 
-//              int bytesAvailable = btSocket.getInputStream().available();
-//              byte []packetBytes= new byte[bytesAvailable];
-//                if (bytesAvailable > 0) {
-//                    String aString = new String(packetBytes, "UTF-8");
-//                    display_data.setText(bytesAvailable+ "ok");
-//                    btSocket.getInputStream().read(packetBytes);
-//
-//                    for(int i=0; i<bytesAvailable;i++)
-//                    {
-//                        if(packetBytes[i]==65)
-//                        display_data.setText(aString);
-//                    }
-//                }
+                for(String s:updates) {
+                    String data="";
+                    data+=variableLabels.get(String.valueOf(s.charAt(0)));
+                    data+="_";
+                    data+=s.charAt(1);
+                    new ApiUbiDots().execute(data);
+                }
+
+
 
 
             } catch (Exception e) {
@@ -175,4 +197,53 @@ public class ledControl extends AppCompatActivity {
             progress.dismiss();
         }
     }
+
+
+
+    public class ApiUbiDots extends AsyncTask<String,Void,Void>{
+
+        private String deviceLabel = "demo-1";
+        private String token = "BBFF-zRLZtutpx4ZGdEO9oSsMNIppHNdZyf";
+        private String endpoint = "https://industrial.api.ubidots.com/api/v1.6/devices";
+        private String userAgent = "Java/0.1";
+
+
+        @Override
+        protected Void doInBackground(String... params) {
+            String ubiEndpoint= endpoint + "/" + deviceLabel;
+            String[] data=params[0].split("_");
+            String variableLabel= data[0];
+            String testValue = data[1];
+            URL ubiUrl = null;
+            try {
+                ubiUrl = new URL(ubiEndpoint);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            String json = "{\"" + variableLabel + "\":" + testValue + "}";
+
+            System.out.println("payload: " + json);
+
+            try{
+                HttpURLConnection con = (HttpURLConnection) ubiUrl.openConnection();
+                con.setRequestMethod("POST");
+                con.setRequestProperty("User-Agent", userAgent);
+                con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                con.setRequestProperty("X-AUTH-TOKEN", token);
+                con.setDoOutput(true);
+                con.setDoInput(true);
+
+                con.setRequestMethod("POST");
+                OutputStream os = con.getOutputStream();
+                os.write(json.getBytes("UTF-8"));
+                int responseCode = con.getResponseCode();
+                System.out.println("response code: " + responseCode);
+            } catch (IOException ex) {
+                Logger.getLogger(ApiUbiDots.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            return null;
+        }
+    }
 }
+
